84dbe8f939adfc36495d4855d7bd4586ff06614c
João Paulo Cercal <jpcercal@gmail.com>
2015-06-28 02:03:25 -0300

    Created the main bundle file.

---
+++ src/Cekurte/WebScrapingBundle/CekurteWebScrapingBundle.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the Cekurte package.
+ *
+ * (c) João Paulo Cercal <jpcercal@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Cekurte\WebScrapingBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CekurteWebScrapingBundle extends Bundle
+{
+}
