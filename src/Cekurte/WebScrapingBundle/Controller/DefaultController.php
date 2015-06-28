08896147b355302b032894f8f04293afa4f1ec0c
João Paulo Cercal <jpcercal@gmail.com>
2015-06-28 02:01:15 -0300

    Added the default controller.

---
+++ src/Cekurte/WebScrapingBundle/Controller/DefaultController.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the Cekurte package.
+ *
+ * (c) João Paulo Cercal <jpcercal@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Cekurte\WebScrapingBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="home")
+     * @Method("GET")
+     *
+     * @return RedirectResponse
+     */
+    public function indexAction()
+    {
+        return new RedirectResponse('https://github.com/jpcercal');
+    }
+}
